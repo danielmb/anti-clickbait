bf4f683bc3cf4007346bec668a1c3bd041bc0125
Daniel Mathisen Breivik <danbre14@nfk.no>
2023-05-02 10:52:50 +0200

    Updated server.yml to move server files and added PM2 restart script

--- server/src/index.ts
+++ server/src/index.ts
@@ -99,6 +99,6 @@ app.use(
   },
 );
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
