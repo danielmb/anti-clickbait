9774a7c2c6f70bd4b24722b7c13b85831ac57026
Daniel Mathisen Breivik <danbre14@nfk.no>
2023-05-02 12:06:20 +0200

    Add environment variable and database URL to server deployment

--- extension/scripts/lib/fetch.js
+++ extension/scripts/lib/fetch.js
@@ -1,7 +1,9 @@
 export async function hentTittel(url) {
   const id = new URL(url).pathname.split('/')[3];
-  const newUrl = new URL('http://localhost:3000/');
+  // const newUrl = new URL('http://localhost:3000/');
+  const newUrl = new URL('http://therack.ddns.net:30000/');
   newUrl.searchParams.append('url', url);
+  newUrl.searchParams.append('styleName', 'clickbait-remover');
   // let response = await fetch('localhost:3000/?url=' + encodeURIComponent(url));
 
   let response = await fetch(newUrl).catch((err) => {
