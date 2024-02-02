0e8a4bce1b686a6b29c7ff01c5a92ac9ce5d73e0
Daniel <danielmbreivik@hotmail.com>
2024-02-02 19:56:00 +0100

    oops

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,6 @@ import https from 'https';
 import cors from 'cors';
 import titleRouter from './routes/title.route';
 import styleRouter from './routes/style.route';
-import receiptRouter from './routes/receipt.route';
 import { errorHandler } from './lib/error';
 import { privateKey, certificate } from './config/ssl';
 const app = express();
@@ -17,7 +16,7 @@ app.use(
 app.use(express.json());
 app.use('/title', titleRouter);
 app.use('/style', styleRouter);
-app.use('/receipt', receiptRouter);
+
 app.use(errorHandler);
 
 const httpsServer = https
